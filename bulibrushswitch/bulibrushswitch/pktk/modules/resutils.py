48fd89a029c33a2ba0067b42be1190723fc916d3
Grum999 <grum999@grum.fr>
2023-12-08 18:10:50 +0100

    Fix bug - resutils
    Path to sqlite db was not correct for windows, fixed

--- bulibrushswitch/bulibrushswitch/pktk/modules/resutils.py
+++ bulibrushswitch/bulibrushswitch/pktk/modules/resutils.py
@@ -287,7 +287,6 @@ class DBManagedResources(QObject):
 
     def __init__(self, fileName, parent=None):
         super(DBManagedResources, self).__init__(parent)
-
         self.__dbInstanceId = f"{DBManagedResources.__DBINSTANCEID}_{QUuid.createUuid().toString()}"
         self.__dbFileName = ""
         self.__databaseInstance = None
@@ -523,7 +522,7 @@ class ManagedResourcesModel(QAbstractTableModel):
         """Initialise list"""
         super(ManagedResourcesModel, self).__init__(parent)
         self.__items = []
-        self.__dbResources = DBManagedResources(os.path.join(QStandardPaths.standardLocations(QStandardPaths.DataLocation)[0], 'resourcecache.sqlite'))
+        self.__dbResources = DBManagedResources(os.path.join(Krita.instance().getAppDataLocation(), 'resourcecache.sqlite'))
         self.__resourceType = None
         self.__displayName = True
 
@@ -680,11 +679,14 @@ class ManagedResourcesModel(QAbstractTableModel):
                             'storageTypeId': query[1].value('storageTypeId'),
                             'storageActive': (query[1].value('storageActive') == 1)
                         }))
-        else:
+        elif query[1]:
             err = query[1].lastError()
             print(err.databaseText())
             print(err.driverText())
             print(err.nativeErrorCode())
+        else:
+            print("Not connected to DB?")
+
         self.endResetModel()
 
     def displayName(self):
